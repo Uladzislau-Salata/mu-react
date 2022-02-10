9b08a2ad767e794226361e5a1e67c7cf408f4e28
Uladzislau-Salata <salata.vlad131997@yandex.ru>
2022-02-10 21:36:24 +0300

    6

--- src/App.js
+++ src/App.js
@@ -2,55 +2,29 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  // const str1 = 'text1';
-  // const str2 = 'text1';
+  // const li1 = <li>text1</li>;
+  // const li2 = <li>text2</li>;
+  // const li3 = <li>text3</li>;
 
-  // const name = "user";
-  // const age = "30";
-
-  // const num1 = 3;
-  // const num2 = 2;
-
-  // const name = 'john';
-  // const surname = 'smit';
-
-  // const num = 4;
-
-  // const arr = [1, 2, 3, 4, 5];
-
-  // const obj = {name: 'john', surname:'smit'};
+  // const items = (
+  //   <>
+  //     <li>text1</li>
+  //     <li>text2</li>
+  //     <li>text3</li>
+  //   </>
+  // );
 
   return (
     <>
-      {/* <p>{str1}</p>
-		<p>{str2}</p>
-    Вставьте первую константу в первый абзац, а вторую константу - во второй. */}
-
-      {/* name: {name} <br></br>     age: {age}
-      Вставьте константу с именем вместо первого знака ?, а константу с возрастом - вместо второго. */}
-
-      {/* result: {num1 ** num2}
-Какой HTML получится в результате рендеринга следующего кода: */}
-
-      {/* result: {name + ' ' + surname}
-Какой HTML получится в результате рендеринга следующего кода: */}
-
-{/* result: {Math.sqrt(num)}
-Какой HTML получится в результате рендеринга следующего кода: */}
-
-{/* <ul>
-  <li>{arr[0]}</li>
-  <li>{arr[1]}</li>
-  <li>{arr[2]}</li>
-  <li>{arr[3]}</li>
-  <li>{arr[4]}</li>
+      {/* <ul>
+  {li1}
+  {li2}
+  {li3}
 </ul>
-Сделайте так, чтобы результатом рендеринга был тег ul, в тегах li которого будут стоять элементы массива. */}
-{/* <p>
-name: <span>{obj.name}</span> <br/> surname: <span>{obj.surname}</span>
-</p>
-Для значений имени и фамилии используйте значения элементов объекта. */}
+Сделайте так, чтобы результатом рендеринга было следующее: */}
 
+      {/* <ul>{items}</ul>
+      Код, однако, не работает. Исправьте ошибку автора кода. */}
 
     </>
   );
