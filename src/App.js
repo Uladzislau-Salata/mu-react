756fb6e57e2b9a03a774818978abbba8f3cbb849
Uladzislau-Salata <salata.vlad131997@yandex.ru>
2022-02-10 20:49:35 +0300

    3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <div>
+      {/* <input defaultValue={1} />
+      <br></br>
+      <input defaultValue={2} />
+      <br></br>
+      <input defaultValue={3} /> */}
+      {/* Добавьте в див несколько инпутов, разделенных тегами br. */}
+
+      {/* <ul>
+<li>1</li>
+<li>2</li>
+<li>3</li>
+<li>4</li>
+<li>5</li>
+<li>6</li>
+<li>7</li>
+<li>8</li>
+<li>9</li>
+<li>10</li>
+</ul>
+Сделайте внутри дива список ul, содержащий в себе 10 тегов li. */}
+{/* <thead>
+  <tr>
+    <td>1</td>
+    <td>2</td>
+    <td>3</td>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>2</td>
+    <td>3</td>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>2</td>
+    <td>3</td>
+  </tr>
+</thead>
+Сделайте внутри дива таблицу с тремя рядами и тремя колонками. */}
+
+{/* <p className="class1">123435</p>
+<p className="class2">123435</p>
+<p className="class3">123435</p>
+Сделайте внутри дива три абзаца с различными CSS классами. */}
+
+    </div>
   );
 }
 
