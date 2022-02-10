e8c91f9804b06475c35d3d516603dcc3dbe1ed21
Uladzislau-Salata <salata.vlad131997@yandex.ru>
2022-02-10 20:59:58 +0300

    4

--- src/App.js
+++ src/App.js
@@ -3,52 +3,30 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      {/* <input defaultValue={1} />
-      <br></br>
-      <input defaultValue={2} />
-      <br></br>
-      <input defaultValue={3} /> */}
-      {/* Добавьте в див несколько инпутов, разделенных тегами br. */}
-
+    <>
       {/* <ul>
-<li>1</li>
-<li>2</li>
-<li>3</li>
-<li>4</li>
-<li>5</li>
-<li>6</li>
-<li>7</li>
-<li>8</li>
-<li>9</li>
-<li>10</li>
-</ul>
-Сделайте внутри дива список ul, содержащий в себе 10 тегов li. */}
-{/* <thead>
-  <tr>
-    <td>1</td>
-    <td>2</td>
-    <td>3</td>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td>2</td>
-    <td>3</td>
-  </tr>
-  <tr>
-    <td>1</td>
-    <td>2</td>
-    <td>3</td>
-  </tr>
-</thead>
-Сделайте внутри дива таблицу с тремя рядами и тремя колонками. */}
+        <li>text1</li>
+        <li>text2</li>
+        <li>text3</li>
+      </ul>
+      Код, однако, не работает. Исправьте ошибку автора кода. */}
+      {/* <ul>
+        <li>text1</li>
+        <li>text2</li>
+        <li>text3</li>
+      </ul>
+      <ul>
+        <li>text1</li>
+        <li>text2</li>
+        <li>text3</li>
+      </ul>
+      Код, однако, не работает. Исправьте ошибку автора кода. Предложите два способа решения проблемы. */}
 
-{/* <p className="class1">123435</p>
-<p className="class2">123435</p>
-<p className="class3">123435</p>
-Сделайте внутри дива три абзаца с различными CSS классами. */}
+ {/* <input/><input/><input/>
+Код, однако, не работает. Исправьте ошибку автора кода. */}
 
-    </div>
+
+    </>
   );
 }
 
