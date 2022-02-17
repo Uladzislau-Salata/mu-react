e4613b343fb0104d8f6af0137937256c5f031926
Uladzislau-Salata <salata.vlad131997@yandex.ru>
2022-02-17 19:30:47 +0300

    7

--- src/App.js
+++ src/App.js
@@ -2,30 +2,35 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  // const li1 = <li>text1</li>;
-  // const li2 = <li>text2</li>;
-  // const li3 = <li>text3</li>;
+  // const attr = "block";
 
-  // const items = (
-  //   <>
-  //     <li>text1</li>
-  //     <li>text2</li>
-  //     <li>text3</li>
-  //   </>
-  // );
+  // const attr = "block";
+
+  // const class1 = "block1";
+  // const class2 = "block2";
+
+  // const str1 = "label";
+  // const str2 = "block";
+  // const str3 = "elem";
 
   return (
     <>
-      {/* <ul>
-  {li1}
-  {li2}
-  {li3}
-</ul>
-Сделайте так, чтобы результатом рендеринга было следующее: */}
+      {/* <div id={attr}>text</div>;
+      Вставьте значение константы attr в атрибут id нашего дива. */}
 
-      {/* <ul>{items}</ul>
+      {/* <div className={attr}>text</div>;
+      Вставьте значение константы attr в атрибут class нашего дива. */}
+
+      {/* <p className={class1}>text</p>
+      <p className={class2}>text</p>
       Код, однако, не работает. Исправьте ошибку автора кода. */}
 
+      {/* <label id={str1} htmlFor={str2} className={str3}>
+        text
+      </label>
+      <br />
+      <input id={str2} />
+      Код не работает. Исправьте ошибки автора кода. */}
     </>
   );
 }
