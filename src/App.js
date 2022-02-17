f15afc626fcec5f4cd407967915996b4bdce18be
Uladzislau-Salata <salata.vlad131997@yandex.ru>
2022-02-17 20:34:57 +0300

    11

--- src/App.js
+++ src/App.js
@@ -2,25 +2,30 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  // function getsum(s) {
-  //   let sum = 0;
-  //   let c = String(s);
-  //   for (let i = 0; i < c.length; i++) {
-  //     sum += Number(c[i]);
-  //   }
-  //   return sum;
+  // function show1() {
+  //   alert(1);
+  // }
+  // function show2() {
+  //   alert(2);
   // }
 
+// function show(elem) {
+//   alert(elem);
+// }
 
+  return (
+    <>
+      {/* <button onClick={show1}>act1</button>
+      <button onClick={show2}>act2</button>
+      Сделайте так, чтобы по клику на первую кнопку срабатывала первая функция, а по клику на вторую кнопку - вторая функция. */}
 
-  return <>
-{/* <p>{getsum(123)}</p>
-Сделайте функцию getDigitsSum, которая будет находить сумму цифр переданного числа. С ее помощью выведите на экран сумму цифр числа 123. */}
+		{/* <button onClick={()=>show('1')}>act1</button>
+		<button onClick={()=>show('2')}>act2</button>
+		<button onClick={()=>show('3')}>act3</button>
+    Сделайте так, чтобы по клику на первую кнопку алертом выводилось число 1, по клику на вторую кнопку - число 2, а по клику на третью - число 3. */}
 
-{/* <p>{getsum(12345)}</p>
-Используя созданную вами функцию getDigitsSum выведите в абзац сумму цифр числа 12345. */}
-
-  </>;
+    </>
+  );
 }
 
 export default App;
