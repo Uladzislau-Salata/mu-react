5ba77d04d975cc10ad31b81f76efea4cfd87a182
Uladzislau-Salata <salata.vlad131997@yandex.ru>
2022-02-17 22:14:22 +0300

    15

--- src/App.js
+++ src/App.js
@@ -1,43 +1,38 @@
 import React from "react";
 import "./App.css";
 
-// function App() {
-//   const users = [
-//     { name: "user1", surn: "surn1", age: 30 },
-//     { name: "user2", surn: "surn2", age: 31 },
-//     { name: "user3", surn: "surn3", age: 32 },
-//   ];
-//   const res = users.map(function (s, index) {
-//     return (
-//       <li key={index}>
-//         {s.name} {s.surn}: {s.age}
-//       </li>
-//     );
-//   });
-
-  //   const users = [
-  //   {id:1, name: "user1", surn: "surn1", age: 30 },
-  //   {id:2, name: "user2", surn: "surn2", age: 31 },
-  //   {id:3, name: "user3", surn: "surn3", age: 32 },
+function App() {
+  // const users = [
+  //   { id: 1, name: "user1", surn: "surn1", age: 30 },
+  //   { id: 2, name: "user2", surn: "surn2", age: 31 },
+  //   { id: 3, name: "user3", surn: "surn3", age: 32 },
   // ];
-  // const res = users.map(function (s) {
+
+  // const rows = users.map(function (s) {
   //   return (
-  //     <li key={s.id}>
-  //       {s.name} {s.surn}: {s.age}
-  //     </li>
+  //     <tr key={s.id}>
+  //       <td>{s.name}</td>
+  //       <td>{s.surn}</td>
+  //       <td>{s.age}</td>
+  //     </tr>
   //   );
   // });
 
   return (
     <>
-      {/* <ul>{res}</ul>
-      Выведите элементы этого массива в виде списка ul. */}
-
-       {/* <ul>{res}</ul>
-       Модифицируйте предыдущую задачу, добавив в массив id и использовав их в качестве значений атрибута key. */}
-
-
+      {/* <table>
+        <thead>
+          <tr>
+            <td>Имя</td>
+            <td>Фамилия</td>
+            <td>Возраст</td>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
     </>
+    Выведите элементы этого массива в виде таблицы table так, чтобы каждое поле объекта попало в свой тег td. Сделайте заголовки колонок вашей таблицы. */}
+
   );
 }
 
