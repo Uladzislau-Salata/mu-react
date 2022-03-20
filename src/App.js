b03228fc2fd1bc3920c6e2f96e1aa772f6269aa9
Uladzislau-Salata <salata.vlad131997@yandex.ru>
2022-03-20 14:50:45 +0300

    21

--- src/App.js
+++ src/App.js
@@ -4,110 +4,91 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  // let transl = [];
-  // transl["А"] = "A";
-  // transl["а"] = "a";
-  // transl["Б"] = "B";
-  // transl["б"] = "b";
-  // transl["В"] = "V";
-  // transl["в"] = "v";
-  // transl["Г"] = "G";
-  // transl["г"] = "g";
-  // transl["Д"] = "D";
-  // transl["д"] = "d";
-  // transl["Е"] = "E";
-  // transl["е"] = "e";
-  // transl["Ё"] = "Yo";
-  // transl["ё"] = "yo";
-  // transl["Ж"] = "Zh";
-  // transl["ж"] = "zh";
-  // transl["З"] = "Z";
-  // transl["з"] = "z";
-  // transl["И"] = "I";
-  // transl["и"] = "i";
-  // transl["Й"] = "J";
-  // transl["й"] = "j";
-  // transl["К"] = "K";
-  // transl["к"] = "k";
-  // transl["Л"] = "L";
-  // transl["л"] = "l";
-  // transl["М"] = "M";
-  // transl["м"] = "m";
-  // transl["Н"] = "N";
-  // transl["н"] = "n";
-  // transl["О"] = "O";
-  // transl["о"] = "o";
-  // transl["П"] = "P";
-  // transl["п"] = "p";
-  // transl["Р"] = "R";
-  // transl["р"] = "r";
-  // transl["С"] = "S";
-  // transl["с"] = "s";
-  // transl["Т"] = "T";
-  // transl["т"] = "t";
-  // transl["У"] = "U";
-  // transl["у"] = "u";
-  // transl["Ф"] = "F";
-  // transl["ф"] = "f";
-  // transl["Х"] = "X";
-  // transl["х"] = "x";
-  // transl["Ц"] = "C";
-  // transl["ц"] = "c";
-  // transl["Ч"] = "Ch";
-  // transl["ч"] = "ch";
-  // transl["Ш"] = "Sh";
-  // transl["ш"] = "sh";
-  // transl["Щ"] = "Shh";
-  // transl["щ"] = "shh";
-  // transl["Ъ"] = '"';
-  // transl["ъ"] = '"';
-  // transl["Ы"] = "Y'";
-  // transl["ы"] = "y'";
-  // transl["Ь"] = "'";
-  // transl["ь"] = "'";
-  // transl["Э"] = "E'";
-  // transl["э"] = "e'";
-  // transl["Ю"] = "Yu";
-  // transl["ю"] = "yu";
-  // transl["Я"] = "Ya";
-  // transl["я"] = "ya";
-  // const [value, setValue] = useState("");
-  // const [value1, setValue1] = useState("");
-  // function translit(event) {
-  //   let a = event.target.value;
-  //   setValue1(a);
-  //   let per = value;
-  //   let elem = a.slice(a.length - 1);
-  //   console.log(transl[elem]);
-  //   if (transl[elem] != undefined) {
-  //     setValue((per += transl[elem]));
+  // const[checked,setChecked]=useState(false);
+  // const[hello,setHello]=useState('');
+  // function hel() {
+  //   if (checked) {
+  //     setHello('Privet');
   //   } else {
-  //     setValue((per += elem));
+  //     setHello('Poka');
   //   }
   // }
 
-  // const [value, setValue] = useState("");
-  // const [value1, setValue1] = useState("");
-  // function sum(event) {
-  //   let valuev = event.target.value;
-  //   setValue(valuev);
-  //   let arr = valuev.split("\n");
-  //   let su = 0;
-  //   for (let a of arr) {
-  //     su += Number(a);
-  //   }
-  //   setValue1(su);
+  // const[checked1,setChecked1]=useState(true);
+  // const[checked2,setChecked2]=useState(true);
+  // const[checked3,setChecked3]=useState(true);
+  // const[text1,setText1]=useState('');
+  // const[text2,setText2]=useState('');
+  // const[text3,setText3]=useState('');
+  // function changeText1() {
+  //   setChecked1(!checked1);
+  //   setText1(String(checked1));
   // }
+  // function changeText2() {
+  //   setChecked2(!checked2);
+  //   setText2(String(checked2));
+  // }
+  // function changeText3() {
+  //   setChecked3(!checked3);
+  //   setText3(String(checked3));
+  // }
+
+  // const[checked1,setChecked1]=useState(true);
+  // let massage;
+  // if (checked1) {
+  // massage=<div>
+  // 	<h2>Ура, вам уже есть 18</h2>
+  // 	<p>
+  // 		здесь расположен контент только для
+  // 			взрослых
+  // 	</p>
+  // </div>;
+  // } else {
+  //   massage=<div>
+  // 	<p>
+  // 		увы, вам еще нет 18 лет:(
+  // 	</p>
+  // </div>;
+  // }
+
+  // const [checked1, setChecked1] = useState(true);
+  // let massage;
+  // if (checked1) {
+  //   massage=<p>1234</p>;
+  // } else {
+  //   massage=<p hidden >1234</p>;
+  // }
+
+
 
   return (
     <>
-      {/* <textarea value={value1} onChange={translit} />
-      <p>{value}</p>
-      Пусть в textarea вводится текст. Сделайте так, чтобы в абзац выводился транслит вводимого текста. */}
+      {/* <input type='checkbox' onChange={()=>setChecked(!checked)} ></input>
+<button onClick={hel}>click</button>
+<p>{hello}</p>
+Дан чекбокс, кнопка и абзац. По клику на кнопку, если чекбокс отмечен, выведите в абзац текст приветствия с пользователем, а если не отмечен - текст прощания. */}
 
-      {/* <textarea value={value} onChange={sum} />
-      <p>{value1}</p>
-      Пусть в textarea на каждой строке вводятся числа. Сделайте так, чтобы по мере ввода в абзац выводилась сумма введенных чисел. */}
+      {/* <p>Отметьте языки которые вы знаете</p>
+<p>HTML<input type='checkbox' onChange={changeText1} ></input></p>
+<p>CSS<input type='checkbox' onChange={changeText2} ></input></p>
+<p>JS<input type='checkbox' onChange={changeText3} ></input></p>
+<p>HTML {text1}</p>
+<p>CSS {text2}</p>
+<p>JS {text3}</p>
+С помощью трех чекбоксов попросите пользователя выбрать языки, которые он знает: html, css и js. Результат выбора по каждому языку выводите в отдельные абзацы. */}
+
+      {/* <input type='checkbox' checked={checked1} onChange={()=>setChecked1(!checked1)} />
+<div>{massage}</div>
+Дан чекбокс. С помощью чекбокса спросите у пользователя, если ли ему уже 18 лет. Если чекбокс отмечен, покажите пользователю следующий блок кода: */}
+
+      {/* <input
+        type="checkbox"
+        checked={checked1}
+        onChange={() => setChecked1(!checked1)}
+      />
+      <div>{massage}</div>
+      Дан чекбокс и абзац. Если чекбокс отмечен, пусть абзац будет видимым на экране, а если не отмечен - спрячьте его. */}
+
 
 
     </>
