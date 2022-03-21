bc84d7b0045331c3e82a6dea685ae4fd5cc9c292
Uladzislau-Salata <salata.vlad131997@yandex.ru>
2022-03-21 23:06:49 +0300

    23

--- src/App.js
+++ src/App.js
@@ -4,50 +4,44 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  // const [valuee, setValuee] = useState("");
-  // function handleChange(ev) {
-  //   setValuee(ev.target.value);
-  // }
-
-  // const [valuee, setValuee] = useState("");
-  // const texts = ["Гродно", "Мосты", "Минск", "Брест"];
-  // const options = texts.map((elem, endex) => {
-  //   return <option key={endex}>{elem}</option>;
-  // });
+// const [valuee,setValuee]=useState();
+// function name(ev) {
+//   setValuee(ev.target.value);
+// }
 
 
-// const [valuee,setValuee]=useState('');
-// const arr=['0-12','13-17','18-25','25+'];
-// const message=arr.map((elem,index)=>{
-//   return <option key={index} value={index}>{elem}</option>;
-// });
+// const [valuee,setValuee]=useState();
+// function name(ev) {
+//   setValuee(ev.target.value);
+//   console.log(valuee);
+// }
+// let message;
+// if (valuee=='3') {
+// message=<p>JS TOP!!!</p>
+// }else if(valuee=='2'){
+//   message=<p>CSS</p>
+// }else if(valuee=='1'){
+//   message=<p>HTML</p>
+// }else{
+// }
+
 
   return (
     <>
-      {/* <div>
-        <select value={valuee} onChange={handleChange}>
-          <option>Мосты</option>
-          <option>Гродно</option>
-          <option>Минск</option>
-          <option>Брест</option>
-        </select>
-      </div>
-      <p>{valuee}</p>
-      Сделайте выпадающий список городов. Сделайте также абзац, в который будет выводиться выбор пользователя. */}
 
-      {/* <select value={valuee} onChange={(ev) => setValuee(ev.target.value)}>
-        {options}
-      </select>
-      <p>Ваш выбор: {valuee}</p>
-      Пусть в массиве хранится список городов. Выведите с помощью цикла выпадающий список этих городов. */}
+{/* <input type='radio' name='radio' value='1' checked={valuee=='1'? true:false} onChange={name} ></input>
+<input type='radio' name='radio' value='2' checked={valuee=='2'? true:false} onChange={name} ></input>
+<input type='radio' name='radio' value='3' checked={valuee=='3'? true:false} onChange={name} ></input>
+<p>{valuee}</p>
+Даны 3 радиокнопочки. Дан абзац. Сделайте так, чтобы значение выбранной радиокнопочки выводилось в этот абзац. */}
 
-{/* <select value={valuee} onChange={ev=>setValuee(ev.target.value)}>
-{message}
-</select>
-<p>
-  Ваш выбор: {arr[valuee]}
-</p>
-С помощью выпадающего списка предложите пользователю выбрать к какой возрастной группе он относится: от 0 до 12 лет, от 13 до 17, от 18 до 25, либо старше 25 лет. */}
+
+{/* <p><input type='radio' name='radio' value='1' checked={valuee=='1'? true:false} onChange={name} ></input>HTML</p>
+<p><input type='radio' name='radio' value='2' checked={valuee=='2'? true:false} onChange={name} ></input>CSS</p>
+<p><input type='radio' name='radio' value='3' checked={valuee=='3'? true:false} onChange={name} ></input>JS</p>
+<div>{message}</div>
+С помощью радиокнопочек спросите у пользователя его любимый язык программирования. Выведите его выбор в абзац. Если выбран язык JavaScript, похвалите пользователя. */}
+
 
 
     </>
