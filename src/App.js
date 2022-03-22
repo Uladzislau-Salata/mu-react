bb748243eeeccd06e28d8b46f41c4aec1a56f982
Uladzislau-Salata <salata.vlad131997@yandex.ru>
2022-03-22 19:49:43 +0300

    24

--- src/App.js
+++ src/App.js
@@ -4,46 +4,15 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-// const [valuee,setValuee]=useState();
-// function name(ev) {
-//   setValuee(ev.target.value);
-// }
+// const [valuee,setValuee]=useState('Start')
 
-
-// const [valuee,setValuee]=useState();
-// function name(ev) {
-//   setValuee(ev.target.value);
-//   console.log(valuee);
-// }
-// let message;
-// if (valuee=='3') {
-// message=<p>JS TOP!!!</p>
-// }else if(valuee=='2'){
-//   message=<p>CSS</p>
-// }else if(valuee=='1'){
-//   message=<p>HTML</p>
-// }else{
-// }
-
+// const [valuee,setValuee]=useState(true)
 
   return (
     <>
+{/* <input type='text' defaultValue={valuee}/> */}
 
-{/* <input type='radio' name='radio' value='1' checked={valuee=='1'? true:false} onChange={name} ></input>
-<input type='radio' name='radio' value='2' checked={valuee=='2'? true:false} onChange={name} ></input>
-<input type='radio' name='radio' value='3' checked={valuee=='3'? true:false} onChange={name} ></input>
-<p>{valuee}</p>
-Даны 3 радиокнопочки. Дан абзац. Сделайте так, чтобы значение выбранной радиокнопочки выводилось в этот абзац. */}
-
-
-{/* <p><input type='radio' name='radio' value='1' checked={valuee=='1'? true:false} onChange={name} ></input>HTML</p>
-<p><input type='radio' name='radio' value='2' checked={valuee=='2'? true:false} onChange={name} ></input>CSS</p>
-<p><input type='radio' name='radio' value='3' checked={valuee=='3'? true:false} onChange={name} ></input>JS</p>
-<div>{message}</div>
-С помощью радиокнопочек спросите у пользователя его любимый язык программирования. Выведите его выбор в абзац. Если выбран язык JavaScript, похвалите пользователя. */}
-
-
-
+{/* <input type='radio' defaultChecked={valuee}/> */}
     </>
   );
 }
