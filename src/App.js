a411b8334d5b6a2f2fe631e629777dff9f26d53b
Uladzislau-Salata <salata.vlad131997@yandex.ru>
2022-03-23 22:08:45 +0300

    26

--- src/App.js
+++ src/App.js
@@ -4,63 +4,40 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-
-// const [arrs,setArrs]=useState([1,2,3,4]);
-// const [valuee,setValuee]=useState('');
-// function added() {
+// const[arrs,setArrs]=useState([1,2,3,4]);
+// const[valuee,setValuee]=useState();
+// const result=arrs.map((elem,index)=>{
+// return <p key={index}>{elem}</p>
+// })
+// function add() {
 //   setArrs([...arrs,valuee]);
 // }
-// const result=arrs.map((elem,index)=>{
-//   return <p key={index} >{elem}</p>;
-// });
-// console.log(valuee);
 
-// const [arrs,setArrs]=useState([1,2,3,4]);
-// let a='1';
-// console.log(arrs);
-// function deled() {
-// // setArrs([...arrs.slice(a,2)])
-// const copy=Object.assign([],arrs);
-// copy.splice(a,1);
-// setArrs(copy);
-// }
-
-// const [arrs,setArrs]=useState([1,2,3,4]);
-// let a='1';
-// console.log(arrs);
-// function deled() {
-// // setArrs([...arrs.slice(a,2)])
-// const copy=Object.assign([],arrs);
-// copy[a]='!';
-// setArrs(copy);
-// }
-
-// const [arrs,setArrs]=useState([4,2,3,1]);
-// // let a='1';
-// console.log(arrs);
-// function deled() {
-// // setArrs([...arrs.slice(a,2)])
-// const copy=Object.assign([],arrs);
-// copy.reverse();
-// setArrs(copy);
+// const [notes,setNotes] =useState(['a', 'b', 'c','d', 'e']);
+// const [valuee,setValuee] =useState();
+// const result=notes.map((elem,index)=>{
+//   return <li key={index} >{elem}</li>
+// })
+// function added() {
+// setNotes([...notes,valuee]);
+// setValuee('');
 // }
 
   return (
     <>
 
-{/* <input type='text' value={valuee} onChange={(ev)=>setValuee(ev.target.value)}/>
-<input type='button' value='click me' onClick={added} />
-{result}
-Сделайте кнопку, по нажатию на которую будет происходить добавление нового элемента в массив. */}
+{/* {result}
+<input type='text' value={valuee} onChange={(ev)=>setValuee(ev.target.value)} />
+<button onClick={add}>Click</button>
+Давайте сделаем инпут и кнопку с помощью которых можно будет реактивно добавлять новые абзацы. */}
 
-{/* <input type='button' value='click me' onClick={deled} />
-Сделайте кнопку, по нажатию на которую будет происходить удаление элемента из массива. Пусть номер элемента для удаления хранится в переменной. */}
+{/* <input type='text' value={valuee} onChange={(ev)=>setValuee(ev.target.value)} onBlur={added}/>
+<ul>
+  {result}
+</ul>
+Выведите элементы этого массива в виде списка ul. Добавьте инпут для добавления новых пунктов списка. Пусть добавление происходит по потери фокуса в инпуте.
+Модифицируйте предыдущую задачу так, чтобы при добавлении новой li текст инпута очищался. */}
 
-{/* <input type='button' value='click me' onClick={deled} />
-Сделайте кнопку, по нажатию на которую будет происходить изменение элемента массива. Пусть номер элемента для изменения хранится в переменной. */}
-
-{/* <input type='button' value='click me' onClick={deled} />
-Сделайте кнопку, по нажатию на которую будет происходить переворот элементов массива в обратном порядке. */}
 
     </>
   );
