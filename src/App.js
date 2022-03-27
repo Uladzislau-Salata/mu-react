ddcd2fe872bd8bd326403e6d0370a7f648dc0fef
Uladzislau-Salata <salata.vlad131997@yandex.ru>
2022-03-27 15:54:35 +0300

    28

--- src/App.js
+++ src/App.js
@@ -4,24 +4,37 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-
-//   const [arrs,setArrs] =useState(['a', 'b', 'c', 'd', 'e']);
-// function dell(s) {
-//   setArrs([...arrs.slice(0,s),...arrs.slice(s+1)]);
-// }
-// const result=arrs.map((elem,index)=>{
-// return <li key={index} >{elem} <button onClick={()=>dell(index)}>click me</button> </li>
-// })
-
+  
+  // const [arrs, setArrs] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  // const result = arrs.map((elem, index) => {
+  //   return (
+  //     <p key={index}>
+  //       <input
+  //         type="number"
+  //         key={index}
+  //         value={elem}
+  //         onChange={(ev) => change(ev, index)}
+  //       />
+  //     </p>
+  //   );
+  // });
+  // function change(ev, i) {
+  //   setArrs([...arrs.slice(0, i), Number(ev.target.value), ...arrs.slice(i + 1)]);
+  // }
+  // function sr(arr) {
+  //   let summ = 0;
+  //   for (let a of arr) {
+  //     summ += a;
+  //   }
+  //   return <p>{summ}</p>;
+  // }
+  // console.log(arrs);
 
   return (
     <>
-    {/* <ul>
-      {result}
-    </ul>
-Выведите элементы этого массива в виде списка ul. Сделайте так, чтобы в конце каждой li стояла кнопка для ее удаления. */}
-
-
+      {/* {result}
+      {sr(arrs)}
+      Выведите на экран среднее арифметическое элементов этого массива. В цикле сделайте инпуты для редактирования элементов. */}
     </>
   );
 }
