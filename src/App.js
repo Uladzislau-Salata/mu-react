86b9f801570bc196ad70bf11b10e4bbf9ade890f
Uladzislau-Salata <salata.vlad131997@yandex.ru>
2022-03-31 20:57:26 +0300

    32

--- src/App.js
+++ src/App.js
@@ -4,19 +4,42 @@ import React, { useState } from "react";
 import { useResolvedPath } from "react-router";
 import "./App.css";
 
+
+const initDate = {
+	year:  2025,
+	month: 12,
+	day:   31,
+}
+
+
 function App() {
-// const [obj,setObj]=useState({prop1:'1',prop2:'2',prop3:'3'})
-// console.log(obj);
+
+// const[obj,setObj]=useState(initDate);
+// const now=new Date();
+// const date= new Date(obj.year, Number(obj.month)-1,obj.day);
+// console.log(date.getDay());
+
+// const[obj,setObj]=useState(initDate);
+// const now=new Date();
+// const date= new Date(obj.year, Number(obj.month)-1,obj.day);
+// console.log(date.getDay());
+// function redac(par,ev) {
+//   setObj({...obj,...{[par]: ev.target.value}})
+// }
+
+  return <>
+
+{/* <p>{obj.day}.{obj.month}.{obj.year}</p>
+Выведите в абзаце год, месяц и день из даты, хранящейся в стейте, а также день недели, соответствующий ей. */}
+
+{/* <p>{obj.day}.{obj.month}.{obj.year}</p>
+<input type='number' value={obj.day} onChange={ev=>redac('day',ev)}></input>
+<input type='number' value={obj.month} onChange={ev=>redac('month',ev)}></input>
+<input type='number' value={obj.year} onChange={ev=>redac('year',ev)}></input>
+Модифицируйте предыдущую задачу, добавив три инпута для редактирования даты. */}
 
 
-  return (
-    <>
-{/* <button onClick={()=>setObj({...obj,...{prop1:'!'}})}>click me1</button>
-<button onClick={()=>setObj({...obj,...{prop2:'!!'}})}>click me2</button>
-<button onClick={()=>setObj({...obj,...{prop3:'!!!'}})}>click me3</button>
-Сделайте 3 кнопки. Пусть первая кнопка изменяет значение свойства prop1, вторая - prop2, а третья - prop3. */}
 
-    </>
-  );
+  </>;
 }
 export default App;
