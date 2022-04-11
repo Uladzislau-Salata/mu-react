7f5b6f3f450adbba4991a437523cd85edd2833a6
Uladzislau-Salata <salata.vlad131997@yandex.ru>
2022-04-11 21:21:56 +0300

    35

--- src/App.js
+++ src/App.js
@@ -42,9 +42,6 @@ function App() {
         <td>{elem.name}</td>
         <td>{elem.catg}</td>
         <td>{elem.cost}</td>
-        <td>
-         <button onClick={()=>dell(elem.id)}>remove</button>
-        </td>
       </tr>
     );
   });
@@ -56,21 +53,57 @@ function App() {
       <td>{arr[2]}</td>
     </tr>
   );
-  function dell(objid) {
-    setIniProds(initProds.filter((el)=> {
-      if (el.id!== objid) {
-        return el
-      }
-    }));
-  }
+
+// function add() {
+//   let newobj={
+//     id: idd(), name: value1, catg: value2, cost: value3
+//   }
+// setIniProds([...initProds,newobj]);
+// setValue1('');
+// setValue2('');
+// setValue3('');
+// }
+// const [value1,setValue1]=useState('');
+// const [value2,setValue2]=useState('');
+// const [value3,setValue3]=useState('');
+
+// function innitobj() {
+//   return{
+//     id:idd(),
+//     name:'',
+//     catg:'',
+//     cost:''
+//   }
+// }
+// const[obji,setObji]=useState(innitobj());
+// function changing (prop,ev) {
+// setObji({...obji,[prop]: ev.target.value})
+// }
+// function added() {
+//   setIniProds([...initProds,obji]);
+//   setObji(innitobj());
+// }
 
   return (
     <>
-      {/* <table>
+      <table>
         <thead>{head}</thead>
         <tbody>{result}</tbody>
       </table>
-      Возьмите таблицу с продуктами initProds. В конце каждого ряда сделайте ячейку, в которой будет кнопка для удаления продукта. */}
+
+{/* <br/>
+<input value={value1} onChange={(ev)=>setValue1(ev.target.value)}/>
+<input value={value2} onChange={(ev)=>setValue2(ev.target.value)}/>
+<input value={value3} onChange={(ev)=>setValue3(ev.target.value)}/>
+<button onClick={add}>add elem</button>
+Возьмите таблицу с продуктами initProds. Сделайте под таблицей инпуты для добавления нового продукта.
+Сделайте так, чтобы текст инпутов очищался после добавления продукто */}
+
+{/* <input value={obji.name} onChange={ev=>changing('name',ev)} />
+<input value={obji.catg} onChange={ev=>changing('catg',ev)} />
+<input value={obji.cost} onChange={ev=>changing('cost',ev)} />
+<button onClick={added}>add elem</button>
+Возьмите таблицу с продуктами initProds. Сделайте под таблицей инпуты для добавления нового продукта. */}
 
     </>
   );
