fbb75834667423974e3899e26cd2fd5fb1f3d7be
Uladzislau-Salata <salata.vlad131997@yandex.ru>
2022-04-18 21:32:45 +0300

    38

--- src/App.js
+++ src/App.js
@@ -4,106 +4,38 @@ import React, { useState } from "react";
 import { useResolvedPath } from "react-router";
 import "./App.css";
 
-const initNotes = [
-  {
-    id: "GYi9G_uC4gBF1e2SixDvu",
-    prop1: "value11",
-    prop2: "value12",
-    prop3: "value13",
-  },
-  {
-    id: "IWSpfBPSV3SXgRF87uO74",
-    prop1: "value21",
-    prop2: "value22",
-    prop3: "value23",
-  },
-  {
-    id: "JAmjRlfQT8rLTm5tG2m1L",
-    prop1: "value31",
-    prop2: "value32",
-    prop3: "value33",
-  },
-];
-
 function App() {
-  const [initProds, setIniProds] = useState([
-    { id: idd(), name: "prod1", catg: "catg1", cost: 100 },
-    { id: idd(), name: "prod2", catg: "catg2", cost: 200 },
-    { id: idd(), name: "prod3", catg: "catg3", cost: 300 },
-  ]);
   function idd(s) {
     let a = nanoid(s);
     return a;
   }
 
-  const result = initProds.map((elem) => {
-    return (
-      <tr key={elem.id}>
-        <td>{elem.name}</td>
-        <td>{elem.catg}</td>
-        <td>{elem.cost}</td>
-        <td>
-          <button onClick={() => setIddd(elem.id)}>choice</button>
-        </td>
-      </tr>
-    );
-  });
-  const arr = Object.keys(initProds[0]).slice(1);
-  const head = (
-    <tr key="heead">
-      <td>{arr[0]}</td>
-      <td>{arr[1]}</td>
-      <td>{arr[2]}</td>
-    </tr>
-  );
-  // const [iddd, setIddd] = useState(null);
-  // function getvalue(prop) {
-  //   if (iddd) {
-  //     return initProds.reduce(
-  //       (res, elem) => (elem.id === iddd ? elem[prop] : res),
-  //       ""
-  //     );
-  //   } else {
-  //     return obj[prop];
-  //   }
-  // }
-  // const [obj, setObj] = useState(getObj());
-  // function getObj() {
-  //   return {
-  //     id: idd(),
-  //     name: "",
-  //     catg: "",
-  //     cost: "",
-  //   };
-  // }
-  // function changing(prop, ev) {
-  //   if (iddd) {
-  //     setIniProds( initProds.map((elem) =>
-  //       elem.id === iddd ? { ...elem, [prop]: ev.target.value } : elem
-  //     ));
-  //   } else {
-  //     setObj({ ...obj, [prop]: ev.target.value });
-  //   }
-  // }
-  // function saveitems() {
-  //   if (iddd) {
-  //     setIddd(null);
-  //   } else {
-  //     setIniProds([...initProds, obj ]);
-  //     setObj(getObj);
-  //   }
-  // }
+  const[visible1,setVisible1]=useState(false);
+  let elem1;
+  if (visible1) {
+    elem1=<p>text</p>;
+  }
+
+  const[visible2,setVisible2]=useState(false);
+  let elem2;
+  if (visible2) {
+    elem2=<p>text</p>;
+  }
+
+  const[visible3,setVisible3]=useState(false);
+  let elem3;
+  if (visible3) {
+    elem3=<p>text</p>;
+  }
   return (
     <>
-      <table>
-        <thead>{head}</thead>
-        <tbody>{result}</tbody>
-      </table>
-      {/* <input value={getvalue("name")} onChange={(ev) => changing("name", ev)} />
-      <input value={getvalue("catg")} onChange={(ev) => changing("catg", ev)} />
-      <input value={getvalue("cost")} onChange={(ev) => changing("cost", ev)} />
-      <button onClick={saveitems}>save</button>
-      Возьмите таблицу с продуктами initProds. Сделайте под таблицей универсальную форму для добавления и редактирования продукта. */}
+    <button onClick={()=>setVisible1(!visible1)}>{visible1?'hide1':'show1'}</button>
+{elem1}
+    <button onClick={()=>setVisible2(!visible2)}>{visible2?'hide2':'show2'}</button>
+{elem2}
+    <button onClick={()=>setVisible3(!visible3)}>{visible3?'hide3':'show3'}</button>
+{elem3}
+Пусть у вас есть три абзаца. Сделайте три кнопки, каждая из которых будет отвечать за показ определенного абзаца
 
     </>
   );
