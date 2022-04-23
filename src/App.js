19057d2fc04956d6beda9c5346023f143c7ae019
Uladzislau-Salata <salata.vlad131997@yandex.ru>
2022-04-23 18:33:28 +0300

    42

--- src/App.js
+++ src/App.js
@@ -9,85 +9,138 @@ function App() {
     let a = nanoid(s);
     return a;
   }
-  // const initNotes = [
-  //   { text: "note1", isEdit: false },
-  //   { text: "note2", isEdit: false },
-  //   { text: "note3", isEdit: false },
-  // ];
-  // const [notes, setNotes] = useState(initNotes);
-  // function startEdit(index) {
-  //   const copy = Object.assign([], notes);
-  //   copy[index].isEdit = true;
-  //   setNotes(copy);
-  // }
-  // function endEdit (index) {
-  //   const copy=Object.assign([],notes);
-  //   copy[index].isEdit=false;
-  //   setNotes(copy);
-  // }
-  // function chang(index, ev) {
-  //   const copy = Object.assign([], notes);
-  //   copy[index].text = ev.target.value;
-  //   setNotes(copy);
-  // }
-  // const result = notes.map((note, index) => {
-  //   let elem;
-  //   if (!note.isEdit) {
-  //     elem = (
-  //       <span onClick={() => startEdit(index)}>
-  //         {note.text}
-  //       </span>
-  //     );
-  //   } else {
-  //     elem = (
-  //       <input
-  //       value={note.text}
-  //       onChange={(ev) => chang(index, ev)}
-  //       onBlur={()=>endEdit(index)}/>
-  //     );
-  //   }
-  //   return <li key={index}>{elem}</li>;
-  // });
-
-  // const initNotes = [
-  //   { text: "note1", isEdit: false },
-  //   { text: "note2", isEdit: false },
-  //   { text: "note3", isEdit: false },
-  // ];
-  // const [notes, setNotes] = useState(initNotes);
-  // const result = notes.map((note, index) => {
-  //   let elem;
-  //   if (!note.isEdit) {
-  //     elem = <p>{note.text}</p>;
-  //   } else {
-  //     elem = <input value={note.text} onChange={(ev) => change(ev, index)} onBlur={()=>evntstartstop(index)} />;
-  //   }
-  //   return (
-  //     <li key={index}>
-  //       {elem}
-  //       <button onClick={() => evntstartstop(index)}>change/accept</button>
-  //     </li>
+  const initNotes = [
+    {
+      id: idd(),
+      fields: [
+        { name: "prop1", value: "value11", isEdit: false },
+        { name: "prop2", value: "value12", isEdit: false },
+        { name: "prop3", value: "value13", isEdit: false },
+      ],
+    },
+    {
+      id: idd(),
+      fields: [
+        { name: "prop1", value: "value21", isEdit: false },
+        { name: "prop2", value: "value22", isEdit: false },
+        { name: "prop3", value: "value23", isEdit: false },
+      ],
+    },
+    {
+      id: idd(),
+      fields: [
+        { name: "prop1", value: "value31", isEdit: false },
+        { name: "prop2", value: "value32", isEdit: false },
+        { name: "prop3", value: "value33", isEdit: false },
+      ],
+    },
+  ];
+  const [notes, setNotes] = useState(initNotes);
+  const rows = notes.map((note) => {
+    const cell = note.fields.map((field) => {
+      let elem;
+      if (!field.isEdit) {
+        elem = <p onClick={() => helper(note.id, field.name)}>{field.value}</p>;
+      } else {
+        elem = (
+          <input
+            value={field.value}
+            onChange={(ev) => helper(note.id, field.name, ev)}
+            onBlur={() => helper(note.id, field.name)}
+          ></input>
+        );
+      }
+      return <td key={field.name}>{elem}</td>;
+    });
+    return <tr key={note.id}>{cell}</tr>;
+  });
+  // function startEdit(id, name) {
+  //   setNotes(
+  //     notes.map((note) => {
+  //       if (note.id === id) {
+  //         const fields = note.fields.map((field) => {
+  //           if (field.name === name) {
+  //             return { ...field, isEdit: true };
+  //           } else {
+  //             return field;
+  //           }
+  //         });
+  //         return { id, fields };
+  //       } else {
+  //         return note;
+  //       }
+  //     })
   //   );
-  // });
-  // function evntstartstop(index) {
-  //   const copy = Object.assign([], notes);
-  //   copy[index].isEdit = !copy[index].isEdit;
-  //   setNotes(copy);
   // }
-  // function change(ev, index) {
-  //   const copy = Object.assign([], notes);
-  //   copy[index].text = ev.target.value;
-  //   setNotes(copy);
+  // function endEdit(id, name) {
+  //   setNotes(
+  //     notes.map((note) => {
+  //       if (note.id === id) {
+  //         const fields = note.fields.map((field) => {
+  //           if (field.name === name) {
+  //             return { ...field, isEdit: false };
+  //           } else {
+  //             return field;
+  //           }
+  //         });
+  //         return { id, fields };
+  //       } else {
+  //         return note;
+  //       }
+  //     })
+  //   );
+  // }
+  // function changeCell(id, name, ev) {
+  //   setNotes(notes.map((note) => {
+  //       if (note.id === id) {
+  //         const fields = note.fields.map((field) => {
+  //           if (field.name === name) {
+  //             return { ...field, value: ev.target.value };
+  //           } else {
+  //             return field;
+  //           }
+  //         });
+  //         return { id, fields };
+  //       } else {
+  //         return note;
+  //       }
+  //     })
+  //   );
+  // }
+
+  // function helper(id, name, ev) {
+  //   setNotes(
+  //     notes.map((note) => {
+  //       if (note.id === id) {
+  //         const fields = note.fields.map((field) => {
+  //           if (field.name === name) {
+  //             if (ev != undefined) {
+  //               return { ...field, value: ev.target.value };
+  //             } else {
+  //               return { ...field, isEdit: !field.isEdit };
+  //             }
+  //           } else {
+  //             return field;
+  //           }
+  //         });
+  //         return { id, fields };
+  //       } else {
+  //         return note;
+  //       }
+  //     })
+  //   );
   // }
 
   return (
     <>
-      {/* <ul>{result}</ul>
-      Давайте выведем этот массив в виде списка ul. Затем сделаем так, чтобы по клику на любую лишку в ней появлялся инпут для редактирования ее текста. */}
+      {/* <table>
+        <tbody>{rows}</tbody>
+      </table>
+      Давайте выведем этот массив в виде HTML таблицы, а затем сделаем так,
+      чтобы по клику на ячейку в ней появлялся инпут для редактирования.
 
-      {/* <ul>{result}</ul>
-      Дан массив. Выведите его в виде списка ul. В конце каждой li сделайте кнопку для редактирования. Пусть по первому нажатию на эту кнопку в тексте li появляется инпут для редактирования, а по второму нажатию - появляется измененный текст. */}
-
+       Функции startEdit, endEdit и changeCell имеют практически полностью идентичный
+      код. Упростите их код, вынеся общую часть в вспомогательную функцию. */}
     </>
   );
 }
